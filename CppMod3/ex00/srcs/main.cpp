a3b5daf3dc01a516bc5b1509d918d06349e23cbc
Tadeo AMIGORENA <tadeo.amigo@gmail.com>
2022-03-14 20:44:30 +0100

    all

--- CppMod3/ex00/srcs/main.cpp
+++ CppMod3/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tamigore <tamigore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 18:28:31 by tamigore          #+#    #+#             */
-/*   Updated: 2022/03/14 19:19:46 by tamigore         ###   ########.fr       */
+/*   Updated: 2022/03/14 20:43:57 by tamigore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 int main()
 {
-    ClapTrap    first();
-    ClapTrap    second;
-    return (0);
+	ClapTrap	first("Tad");
+	ClapTrap	second;
+	ClapTrap	third = first;
+
+	second.attack("Tad");
+	first.takeDamage(second.getData('D'));
+	third.takeDamage(second.getData('D'));
+	first.beRepaired(first.getData('E'));
+	third.beRepaired(100);
+	second.beRepaired(1);
+	return (0);
 }
